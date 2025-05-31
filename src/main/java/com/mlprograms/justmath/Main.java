30334d285d62fd3c220d099c33ad8198c996bb17
Max Lemberg <max.lemberg2402@gmail.com>
2025-05-31 16:45:14 +0200

    add Main class to execute calculations using CalculatorEngine

---
+++ src/main/java/com/mlprograms/justmath/Main.java
@@ -0,0 +1,14 @@
+package com.mlprograms.justmath;
+
+import com.mlprograms.justmath.calculator.CalculatorEngine;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		CalculatorEngine calculatorEngine = new CalculatorEngine(1);
+		System.out.println(calculatorEngine.calculate("((2.5 + sqrt(16) * (3 - 1)^2) / (4.2 - 1.1))^2 + (5 - 6 / 2 + 3^2 - (1.5 + 2.5 / 5)) * (7 + sqrt(49)) / 2^3 - sqrt(81) + 0.5^(2 + 1)"));
+
+	}
+
+}
