c0613463169a06f4bc88af1626e4440d4f28ac5c
Max Lemberg <max.lemberg2402@gmail.com>
2025-06-06 17:46:09 +0200

    deleted MathFunctions.java -> no need

--- src/main/java/com/mlprograms/justmath/bignumber/internal/ArithmeticOperator.java
+++ src/main/java/com/mlprograms/justmath/bignumber/internal/ArithmeticOperator.java
@@ -11,31 +11,54 @@ public enum ArithmeticOperator {
 
 	LEFT_PARENTHESIS("(", false, 0),
 	RIGHT_PARENTHESIS(")", false, 0),
+
 	ADD("+", false, 2),
 	SUBTRACT("-", false, 2),
 	MULTIPLY("*", false, 3),
 	DIVIDE("/", false, 3),
 	POWER("^", false, 4),
+
 	ROOT_S("√", true, 4),
 	ROOT_T("sqrt", true, 4),
+	NTH_ROOT("root", true, 4),
 	CUBIC_ROOT_S("³√", true, 4),
 	CUBIC_ROOT_T("cbrt", true, 4),
+
 	FACTORIAL("!", true, 5),
+
 	SIN("sin", true, 6),
 	COS("cos", true, 6),
 	TAN("tan", true, 6),
+	COT("cot", true, 6),
+
 	SINH("sinh", true, 6),
 	COSH("cosh", true, 6),
 	TANH("tanh", true, 6),
-	ASIN("sin⁻¹", true, 6),
-	ACOS("cos⁻¹", true, 6),
-	ATAN("tan⁻¹", true, 6),
-	ASINH("sinh⁻¹", true, 6),
-	ACOSH("cosh⁻¹", true, 6),
-	ATANH("tanh⁻¹", true, 6),
+	COTH("coth", true, 6),
+
+	ASIN_S("sin⁻¹", true, 6),
+	ASIN_T("asin", true, 6),
+	ACOS_S("cos⁻¹", true, 6),
+	ACOS_T("acos", true, 6),
+	ATAN_S("tan⁻¹", true, 6),
+	ATAN_T("atan", true, 6),
+	ACOT_S("cot⁻¹", true, 6),
+	ACOT_T("acot", true, 6),
+
+	ASINH_S("sinh⁻¹", true, 6),
+	ASINH_T("asinh", true, 6),
+	ACOSH_S("cosh⁻¹", true, 6),
+	ACOSH_T("acosh", true, 6),
+	ATANH_S("tanh⁻¹", true, 6),
+	ATANH_T("atanh", true, 6),
+	ACOTH_S("coth⁻¹", true, 6),
+	ACOTH_T("acoth", true, 6),
+
 	LOG10("log", true, 6),
 	LN("ln", true, 6),
-	LOG_BASE("log", true, 6);
+	LOG_BASE("logbase", true, 6),
+
+	ATAN2("atan2", true, 6); // TODO: two arguments
 
 	private final String operator;
 	private final boolean isFunction;
