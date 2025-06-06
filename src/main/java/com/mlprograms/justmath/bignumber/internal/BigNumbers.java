64fd9deace6c2b2f33f7b40feab19c2d3ceef720
Max Lemberg <max.lemberg2402@gmail.com>
2025-06-06 17:47:06 +0200

    add BigNumbers class to define constant BigNumber values

---
+++ src/main/java/com/mlprograms/justmath/bignumber/internal/BigNumbers.java
@@ -0,0 +1,11 @@
+package com.mlprograms.justmath.bignumber.internal;
+
+import com.mlprograms.justmath.bignumber.BigNumber;
+
+public class BigNumbers {
+
+	public static final BigNumber TWO = new BigNumber("2");
+	public static final BigNumber ONE = new BigNumber("1");
+	public static final BigNumber ZERO = new BigNumber("0");
+
+}
