44831c340ab8e9bf227434bf0eecca8f346595ca
Max Lemberg <max.lemberg2402@gmail.com>
2025-06-07 20:33:30 +0200

    enhanced applyOperator and applyFunction methods in Evaluator; added MODULO operator support and improved argument handling for mathematical functions

--- src/main/java/com/mlprograms/justmath/calculator/internal/Evaluator.java
+++ src/main/java/com/mlprograms/justmath/calculator/internal/Evaluator.java
@@ -2,7 +2,6 @@ package com.mlprograms.justmath.calculator.internal;
 
 import com.mlprograms.justmath.bignumber.BigNumber;
 import com.mlprograms.justmath.bignumber.internal.ArithmeticOperator;
-import com.mlprograms.justmath.bignumber.internal.BigNumbers;
 import com.mlprograms.justmath.calculator.internal.token.Token;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -61,96 +60,102 @@ public class Evaluator {
 	 * 	if division by zero occurs
 	 */
 	private void applyOperator(ArithmeticOperator op, Deque<BigNumber> stack) {
+		BigNumber b = stack.pop();
+		BigNumber a = stack.pop();
+
 		switch (op) {
-			case ADD -> stack.push(stack.pop().add(stack.pop()));
-			case SUBTRACT -> {
-				BigNumber b = stack.pop();
-				BigNumber a = stack.pop();
-				stack.push(a.subtract(b));
-			}
-			case MULTIPLY -> stack.push(stack.pop().multiply(stack.pop()));
-			case DIVIDE -> {
-				BigNumber b = stack.pop();
-				BigNumber a = stack.pop();
-				if (b.compareTo(BigNumbers.ZERO) == 0) {
-					throw new ArithmeticException("Division by zero");
-				}
-				stack.push(a.divide(b, mathContext));
-			}
-			case POWER -> {
-				BigNumber exponent = stack.pop();
-				BigNumber base = stack.pop();
-				stack.push(base.power(exponent, mathContext, CALCULATION_LOCALE));
-			}
-			case PERMUTATION_S -> {
-				BigNumber k = stack.pop();
-				BigNumber n = stack.pop();
-				stack.push(n.permutation(k, mathContext, CALCULATION_LOCALE));
-			}
-			case COMBINATION_S -> {
-				BigNumber k = stack.pop();
-				BigNumber n = stack.pop();
-				stack.push(n.combination(k, mathContext));
-			}
+			case ADD -> stack.push(a.add(b));
+			case SUBTRACT -> stack.push(a.subtract(b));
+			case MULTIPLY -> stack.push(a.multiply(b));
+			case DIVIDE -> stack.push(a.divide(b, mathContext));
+			case POWER -> stack.push(a.power(b, mathContext, CALCULATION_LOCALE));
+			case PERMUTATION_S -> stack.push(a.permutation(b, mathContext, CALCULATION_LOCALE));
+			case COMBINATION_S -> stack.push(a.combination(b, mathContext));
+			case MODULO -> stack.push(a.modulo(b));
 			default -> throw new IllegalArgumentException("Unknown operator: " + op);
 		}
 	}
 
 	/**
-	 * Applies a mathematical function to the top value on the stack.
+	 * Applies a mathematical function to one or more {@link BigNumber} arguments on the stack,
+	 * depending on the specified {@link ArithmeticOperator}.
+	 * <p>
+	 * Most functions operate on a single argument popped from the top of the stack.
+	 * Some functions (e.g. {@code LOG_BASE}, {@code NTH_ROOT}, {@code ATAN2}, {@code PERMUTATION_T},
+	 * {@code COMBINATION_T})
+	 * require two arguments, which are popped in reverse order (i.e. second argument first, then first argument).
+	 * <p>
+	 * The result of the function is pushed back onto the stack.
 	 *
 	 * @param func
-	 * 	the function name as a string (e.g., "sin", "cos", "tan", "log", "sqrt")
+	 * 	the function to apply; must be a valid {@link ArithmeticOperator} representing a function
 	 * @param stack
-	 * 	the stack containing operands as BigDecimal values; the argument is popped from the stack
+	 * 	the operand stack containing one or more {@link BigNumber} values
 	 *
 	 * @throws IllegalArgumentException
-	 * 	if the function is unknown
+	 * 	if the function is unknown or unsupported
 	 */
 	private void applyFunction(@NonNull ArithmeticOperator func, Deque<BigNumber> stack) {
-		BigNumber arg = stack.pop();
+		BigNumber x = stack.pop(); // all functions require at least one argument (x)
+
 		switch (func) {
-			case SIN -> stack.push(arg.sin(mathContext, trigonometricMode, CALCULATION_LOCALE));
-			case COS -> stack.push(arg.cos(mathContext, trigonometricMode, CALCULATION_LOCALE));
-			case TAN -> stack.push(arg.tan(mathContext, trigonometricMode, CALCULATION_LOCALE));
-			case COT -> stack.push(arg.cot(mathContext, CALCULATION_LOCALE));
-			case SINH -> stack.push(arg.sinh(mathContext, CALCULATION_LOCALE));
-			case COSH -> stack.push(arg.cosh(mathContext, CALCULATION_LOCALE));
-			case TANH -> stack.push(arg.tanh(mathContext, CALCULATION_LOCALE));
-			case COTH -> stack.push(arg.coth(mathContext, CALCULATION_LOCALE));
-			case ASIN_S, ASIN_T -> stack.push(arg.asin(mathContext, trigonometricMode, CALCULATION_LOCALE));
-			case ACOS_S, ACOS_T -> stack.push(arg.acos(mathContext, trigonometricMode, CALCULATION_LOCALE));
-			case ATAN_S, ATAN_T -> stack.push(arg.atan(mathContext, trigonometricMode, CALCULATION_LOCALE));
-			case ACOT_S, ACOT_T -> stack.push(arg.acot(mathContext, CALCULATION_LOCALE));
-			case ASINH_S, ASINH_T -> stack.push(arg.asinh(mathContext, CALCULATION_LOCALE));
-			case ACOSH_S, ACOSH_T -> stack.push(arg.acosh(mathContext, CALCULATION_LOCALE));
-			case ATANH_S, ATANH_T -> stack.push(arg.atanh(mathContext, CALCULATION_LOCALE));
-			case ACOTH_S, ACOTH_T -> stack.push(arg.acoth(mathContext, CALCULATION_LOCALE));
-			case LOG10 -> stack.push(arg.log10(mathContext, CALCULATION_LOCALE));
-			case LOG2 -> stack.push(arg.log2(mathContext, CALCULATION_LOCALE));
-			case LN -> stack.push(arg.ln(mathContext, CALCULATION_LOCALE));
+			case SIN -> stack.push(x.sin(mathContext, trigonometricMode, CALCULATION_LOCALE));
+			case COS -> stack.push(x.cos(mathContext, trigonometricMode, CALCULATION_LOCALE));
+			case TAN -> stack.push(x.tan(mathContext, trigonometricMode, CALCULATION_LOCALE));
+			case COT -> stack.push(x.cot(mathContext, CALCULATION_LOCALE));
+			case SINH -> stack.push(x.sinh(mathContext, CALCULATION_LOCALE));
+			case COSH -> stack.push(x.cosh(mathContext, CALCULATION_LOCALE));
+			case TANH -> stack.push(x.tanh(mathContext, CALCULATION_LOCALE));
+			case COTH -> stack.push(x.coth(mathContext, CALCULATION_LOCALE));
+			case ASIN_S, ASIN_T -> stack.push(x.asin(mathContext, trigonometricMode, CALCULATION_LOCALE));
+			case ACOS_S, ACOS_T -> stack.push(x.acos(mathContext, trigonometricMode, CALCULATION_LOCALE));
+			case ATAN_S, ATAN_T -> stack.push(x.atan(mathContext, trigonometricMode, CALCULATION_LOCALE));
+			case ACOT_S, ACOT_T -> stack.push(x.acot(mathContext, CALCULATION_LOCALE));
+			case ASINH_S, ASINH_T -> stack.push(x.asinh(mathContext, CALCULATION_LOCALE));
+			case ACOSH_S, ACOSH_T -> stack.push(x.acosh(mathContext, CALCULATION_LOCALE));
+			case ATANH_S, ATANH_T -> stack.push(x.atanh(mathContext, CALCULATION_LOCALE));
+			case ACOTH_S, ACOTH_T -> stack.push(x.acoth(mathContext, CALCULATION_LOCALE));
+			case LOG10 -> stack.push(x.log10(mathContext, CALCULATION_LOCALE));
+			case LOG2 -> stack.push(x.log2(mathContext, CALCULATION_LOCALE));
+			case LN -> stack.push(x.ln(mathContext, CALCULATION_LOCALE));
 			case LOG_BASE -> {
-				BigNumber base = stack.pop(); // logBase(base, x)
-				stack.push(arg.logBase(base, mathContext, CALCULATION_LOCALE));
+				BigNumber base = stack.pop();
+				stack.push(x.logBase(base, mathContext, CALCULATION_LOCALE));
 			}
-			case ROOT_S, ROOT_T -> stack.push(arg.squareRoot(mathContext, CALCULATION_LOCALE));
-			case CUBIC_ROOT_S, CUBIC_ROOT_T -> stack.push(arg.cubicRoot(mathContext, CALCULATION_LOCALE));
+			case ROOT_S, ROOT_T -> stack.push(x.squareRoot(mathContext, CALCULATION_LOCALE));
+			case CUBIC_ROOT_S, CUBIC_ROOT_T -> stack.push(x.cubicRoot(mathContext, CALCULATION_LOCALE));
 			case NTH_ROOT -> {
 				BigNumber n = stack.pop();
-				stack.push(arg.nthRoot(n, mathContext, CALCULATION_LOCALE));
+				stack.push(x.nthRoot(n, mathContext, CALCULATION_LOCALE));
 			}
-			case FACTORIAL -> stack.push(arg.factorial(mathContext, CALCULATION_LOCALE));
+			case FACTORIAL -> stack.push(x.factorial(mathContext, CALCULATION_LOCALE));
 			case ATAN2 -> {
 				BigNumber y = stack.pop();
-				stack.push(arg.atan2(y, mathContext, CALCULATION_LOCALE));
+				stack.push(x.atan2(y, mathContext, CALCULATION_LOCALE));
 			}
 			case PERMUTATION_T -> {
 				BigNumber n = stack.pop();
-				stack.push(n.permutation(arg, mathContext, CALCULATION_LOCALE));
+				stack.push(n.permutation(x, mathContext, CALCULATION_LOCALE));
 			}
 			case COMBINATION_T -> {
 				BigNumber n = stack.pop();
-				stack.push(n.combination(arg, mathContext));
+				stack.push(n.combination(x, mathContext));
+			}
+			case POLAR_TO_CARTESIAN -> {
+				// TODO: implement this logic
+				throw new RuntimeException("this logic is not implemented yet!");
+			}
+			case CARTESIAN_TO_POLAR -> {
+				// TODO: implement this logic
+				throw new RuntimeException("this logic is not implemented yet!");
+			}
+			case GCD -> {
+				// TODO: implement this logic
+				throw new RuntimeException("this logic is not implemented yet!");
+			}
+			case LCM -> {
+				// TODO: implement this logic
+				throw new RuntimeException("this logic is not implemented yet!");
 			}
 			default -> throw new IllegalArgumentException("Unknown function: " + func);
 		}
