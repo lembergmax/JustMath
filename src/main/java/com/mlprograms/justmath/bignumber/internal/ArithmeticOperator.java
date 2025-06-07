e2c798a05d6f37621a4851fe488ecb09d724ba76
Max Lemberg <max.lemberg2402@gmail.com>
2025-06-07 23:04:01 +0200

    refactored ArithmeticOperator enum; updated operator names to include suffixes for operand count and improved structure for advanced mathematical operations

--- src/main/java/com/mlprograms/justmath/bignumber/internal/ArithmeticOperator.java
+++ src/main/java/com/mlprograms/justmath/bignumber/internal/ArithmeticOperator.java
@@ -9,74 +9,78 @@ import java.util.Optional;
 @AllArgsConstructor
 public enum ArithmeticOperator {
 
-	LEFT_PARENTHESIS("(", false, 0),
-	RIGHT_PARENTHESIS(")", false, 0),
-	SEMICOLON(";", false, 0),
+	// name description: NAME_<CONFIGURATION>_<OPERAND_COUNT(>=2)>
+	// CONFIGURATION: A = Advanced (not only basic letters, numbers or symbols), F = Function, O = Operator (can consist of multiple letters)
 
-	ADD("+", false, 2),
-	SUBTRACT("-", false, 2),
-	MULTIPLY("*", false, 3),
-	DIVIDE("/", false, 3),
-	MODULO("%", false, 3),
-	POWER("^", false, 4),
+	LEFT_PARENTHESIS("(", false, 0, 0),
+	RIGHT_PARENTHESIS(")", false, 0, 0),
+	SEMICOLON(";", false, 0, 0),
 
-	ROOT_S("√", true, 4),
-	ROOT_T("sqrt", true, 4),
-	NTH_ROOT("root", true, 4),
-	CUBIC_ROOT_S("³√", true, 4),
-	CUBIC_ROOT_T("cbrt", true, 4),
+	ADD_O("+", false, 2, 0),
+	SUBTRACT_O("-", false, 2, 0),
+	MULTIPLY_O("*", false, 3, 0),
+	DIVIDE_O("/", false, 3, 0),
+	MODULO_O("%", false, 3, 0),
+	POWER_O("^", false, 4, 0),
+	FACTORIAL_O("!", true, 5, 1),
 
-	FACTORIAL("!", true, 5),
+	ROOT_A("√", true, 4, 1),
+	ROOT_F("sqrt", true, 4, 1),
 
-	SIN("sin", true, 6),
-	COS("cos", true, 6),
-	TAN("tan", true, 6),
-	COT("cot", true, 6),
+	NTH_ROOT_F2("rootn", true, 4, 2),
+	CUBIC_ROOT_AF("³√", true, 4, 1),
+	CUBIC_ROOT_F("cbrt", true, 4, 1),
 
-	SINH("sinh", true, 6),
-	COSH("cosh", true, 6),
-	TANH("tanh", true, 6),
-	COTH("coth", true, 6),
+	SIN_F("sin", true, 6, 1),
+	COS_F("cos", true, 6, 1),
+	TAN_F("tan", true, 6, 1),
+	COT_F("cot", true, 6, 1),
 
-	ASIN_S("sin⁻¹", true, 6),
-	ASIN_T("asin", true, 6),
-	ACOS_S("cos⁻¹", true, 6),
-	ACOS_T("acos", true, 6),
-	ATAN_S("tan⁻¹", true, 6),
-	ATAN_T("atan", true, 6),
-	ACOT_S("cot⁻¹", true, 6),
-	ACOT_T("acot", true, 6),
+	SINH_F("sinh", true, 6, 1),
+	COSH_F("cosh", true, 6, 1),
+	TANH_F("tanh", true, 6, 1),
+	COTH_F("coth", true, 6, 1),
 
-	ASINH_S("sinh⁻¹", true, 6),
-	ASINH_T("asinh", true, 6),
-	ACOSH_S("cosh⁻¹", true, 6),
-	ACOSH_T("acosh", true, 6),
-	ATANH_S("tanh⁻¹", true, 6),
-	ATANH_T("atanh", true, 6),
-	ACOTH_S("coth⁻¹", true, 6),
-	ACOTH_T("acoth", true, 6),
+	ASIN_AF("sin⁻¹", true, 6, 1),
+	ASIN_F("asin", true, 6, 1),
+	ACOS_AF("cos⁻¹", true, 6, 1),
+	ACOS_F("acos", true, 6, 1),
+	ATAN_AF("tan⁻¹", true, 6, 1),
+	ATAN_F("atan", true, 6, 1),
+	ACOT_AF("cot⁻¹", true, 6, 1),
+	ACOT_F("acot", true, 6, 1),
 
-	LOG2("log2", true, 6),
-	LOG10("log", true, 6),
-	LN("ln", true, 6),
-	LOG_BASE("logbase", true, 6),
+	ASINH_AF("sinh⁻¹", true, 6, 1),
+	ASINH_F("asinh", true, 6, 1),
+	ACOSH_AF("cosh⁻¹", true, 6, 1),
+	ACOSH_F("acosh", true, 6, 1),
+	ATANH_AF("tanh⁻¹", true, 6, 1),
+	ATANH_F("atanh", true, 6, 1),
+	ACOTH_AF("coth⁻¹", true, 6, 1),
+	ACOTH_F("acoth", true, 6, 1),
 
-	ATAN2("atan2", true, 6),
+	LOG2_F("log2", true, 6, 1),
+	LOG10_F("log", true, 6, 1),
+	LN_F("ln", true, 6, 1),
+	LOG_BASE_F2("logbase", true, 6, 2),
 
-	PERMUTATION_S("nPr", false, 6),
-	PERMUTATION_T("perm", true, 6),
-	COMBINATION_S("nCr", false, 6),
-	COMBINATION_T("comb", true, 6),
+	ATAN2_F2("atan2", true, 6, 2),
 
-	POLAR_TO_CARTESIAN("Pol", true, 6),
-	CARTESIAN_TO_POLAR("Rec", true, 6),
+	PERMUTATION_O("nPr", false, 6, 1),
+	PERMUTATION_F2("perm", true, 6, 2),
+	COMBINATION_O("nCr", false, 6, 1),
+	COMBINATION_F2("comb", true, 6, 2),
 
-	GCD("GCD", true, 6),
-	LCM("LCM", true, 6);
+	POLARTOCARTESIAN_F2("Pol", true, 6, 2),
+	CARTESIANTOPOLAR_F2("Rec", true, 6, 2),
+
+	GCD_F2("GCD", true, 6, 2),
+	LCM_F2("LCM", true, 6, 2);
 
 	private final String operator;
 	private final boolean isFunction;
 	private final int precedence;
+	private final int requiredOperandsCount;
 
 	/**
 	 * Finds an {@link ArithmeticOperator} by its operator string.
