85e85e76ace0f917331944ec08f4af776dd4061f
Max Lemberg <max.lemberg2402@gmail.com>
2025-06-08 22:52:21 +0200

    added InverseTrigonometricMath class with methods for asin, acos, atan, and acot

---
+++ src/main/java/com/mlprograms/justmath/bignumber/internal/calculator/InverseTrigonometricMath.java
@@ -0,0 +1,43 @@
+package com.mlprograms.justmath.bignumber.internal.calculator;
+
+import ch.obermuhlner.math.big.BigDecimalMath;
+import com.mlprograms.justmath.bignumber.BigNumber;
+import com.mlprograms.justmath.calculator.internal.TrigonometricMode;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.Locale;
+
+import static com.mlprograms.justmath.bignumber.internal.calculator.MathHelper.bigDecimalRadiansToDegrees;
+
+public class InverseTrigonometricMath {
+
+	public static BigNumber asin(BigNumber argument, MathContext mathContext, TrigonometricMode trigonometricMode, Locale locale) {
+		BigDecimal result = BigDecimalMath.asin(argument.toBigDecimal(), mathContext);
+		if (trigonometricMode == TrigonometricMode.DEG) {
+			result = bigDecimalRadiansToDegrees(result, mathContext, locale);
+		}
+		return new BigNumber(result.toPlainString(), locale);
+	}
+
+	public static BigNumber acos(BigNumber argument, MathContext mathContext, TrigonometricMode trigonometricMode, Locale locale) {
+		BigDecimal result = BigDecimalMath.acos(argument.toBigDecimal(), mathContext);
+		if (trigonometricMode == TrigonometricMode.DEG) {
+			result = bigDecimalRadiansToDegrees(result, mathContext, locale);
+		}
+		return new BigNumber(result.toPlainString(), locale);
+	}
+
+	public static BigNumber atan(BigNumber argument, MathContext mathContext, TrigonometricMode trigonometricMode, Locale locale) {
+		BigDecimal result = BigDecimalMath.atan(argument.toBigDecimal(), mathContext);
+		if (trigonometricMode == TrigonometricMode.DEG) {
+			result = bigDecimalRadiansToDegrees(result, mathContext, locale);
+		}
+		return new BigNumber(result.toPlainString(), locale);
+	}
+
+	public static BigNumber acot(BigNumber argument, MathContext mathContext, Locale locale) {
+		return new BigNumber(BigDecimalMath.acot(argument.toBigDecimal(), mathContext).toPlainString(), locale);
+	}
+
+}
