bba2696af3ab563843c4e930974470e18fe2c778
Max Lemberg <max.lemberg2402@gmail.com>
2025-06-08 22:52:40 +0200

    added TrigonometricMath class with methods for sin, cos, tan, and cot calculations

---
+++ src/main/java/com/mlprograms/justmath/bignumber/internal/calculator/TrigonometricMath.java
@@ -0,0 +1,35 @@
+package com.mlprograms.justmath.bignumber.internal.calculator;
+
+import ch.obermuhlner.math.big.BigDecimalMath;
+import com.mlprograms.justmath.bignumber.BigNumber;
+import com.mlprograms.justmath.calculator.internal.TrigonometricMode;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.Locale;
+
+import static com.mlprograms.justmath.bignumber.internal.calculator.MathHelper.convertAngle;
+
+public class TrigonometricMath {
+
+	public static BigNumber sin(BigNumber angle, MathContext mathContext, TrigonometricMode trigonometricMode, Locale locale) {
+		BigDecimal radians = convertAngle(angle, mathContext, trigonometricMode, locale);
+		return new BigNumber(BigDecimalMath.sin(radians, mathContext).toPlainString(), locale);
+	}
+
+	public static BigNumber cos(BigNumber angle, MathContext mathContext, TrigonometricMode trigonometricMode, Locale locale) {
+		BigDecimal radians = convertAngle(angle, mathContext, trigonometricMode, locale);
+		return new BigNumber(BigDecimalMath.cos(radians, mathContext).toPlainString(), locale);
+	}
+
+	public static BigNumber tan(BigNumber angle, MathContext mathContext, TrigonometricMode trigonometricMode, Locale locale) {
+		BigDecimal radians = convertAngle(angle, mathContext, trigonometricMode, locale);
+		return new BigNumber(BigDecimalMath.tan(radians, mathContext).toPlainString(), locale);
+	}
+
+	public static BigNumber cot(BigNumber angle, MathContext mathContext, TrigonometricMode trigonometricMode, Locale locale) {
+		BigDecimal radians = convertAngle(angle, mathContext, trigonometricMode, locale);
+		return new BigNumber(BigDecimalMath.cot(radians, mathContext).toPlainString(), locale);
+	}
+
+}
