98205434f3246235292a27b4bea380301d058eff
Max Lemberg <max.lemberg2402@gmail.com>
2025-06-08 12:55:12 +0200

    refactored BigNumberTest; added structure for comprehensive mathematical operation tests

--- src/test/java/com/mlprograms/justmath/bignumber/BigNumberTest.java
+++ src/test/java/com/mlprograms/justmath/bignumber/BigNumberTest.java
@@ -1,12 +1,7 @@
 package com.mlprograms.justmath.bignumber;
 
-import jdk.jfr.Name;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Locale;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BigNumberTest {
 
@@ -20,16 +15,423 @@ public class BigNumberTest {
 	}
 
 	@Test
-	@Name("Test BigNumber Addition")
-	void additionTest() {
-		BigNumber result = bigNumber1.add(bigNumber2);
-		assertEquals("777777", result.toString());
+	void add() {
+	}
 
-		bigNumber1 = new BigNumber("0.000000000000001", Locale.US);
-		bigNumber2 = new BigNumber("0.000000000000002", Locale.US);
+	@Test
+	void subtract() {
+	}
 
-		result = bigNumber1.add(bigNumber2);
-		assertEquals("0.000000000000003", result.toString());
+	@Test
+	void multiply() {
+	}
+
+	@Test
+	void divide() {
+	}
+
+	@Test
+	void testDivide() {
+	}
+
+	@Test
+	void power() {
+	}
+
+	@Test
+	void testPower() {
+	}
+
+	@Test
+	void squareRoot() {
+	}
+
+	@Test
+	void testSquareRoot() {
+	}
+
+	@Test
+	void cubicRoot() {
+	}
+
+	@Test
+	void testCubicRoot() {
+	}
+
+	@Test
+	void nthRoot() {
+	}
+
+	@Test
+	void testNthRoot() {
+	}
+
+	@Test
+	void factorial() {
+	}
+
+	@Test
+	void testFactorial() {
+	}
+
+	@Test
+	void log2() {
+	}
+
+	@Test
+	void testLog2() {
+	}
+
+	@Test
+	void log10() {
+	}
+
+	@Test
+	void testLog10() {
+	}
+
+	@Test
+	void ln() {
+	}
+
+	@Test
+	void testLn() {
+	}
+
+	@Test
+	void logBase() {
+	}
+
+	@Test
+	void testLogBase() {
+	}
+
+	@Test
+	void sin() {
+	}
+
+	@Test
+	void testSin() {
+	}
+
+	@Test
+	void testSin1() {
+	}
+
+	@Test
+	void cos() {
+	}
+
+	@Test
+	void testCos() {
+	}
+
+	@Test
+	void testCos1() {
+	}
+
+	@Test
+	void tan() {
+	}
+
+	@Test
+	void testTan() {
+	}
+
+	@Test
+	void testTan1() {
+	}
+
+	@Test
+	void sinh() {
+	}
+
+	@Test
+	void testSinh() {
+	}
+
+	@Test
+	void cosh() {
+	}
+
+	@Test
+	void testCosh() {
+	}
+
+	@Test
+	void tanh() {
+	}
+
+	@Test
+	void testTanh() {
+	}
+
+	@Test
+	void asin() {
+	}
+
+	@Test
+	void testAsin() {
+	}
+
+	@Test
+	void testAsin1() {
+	}
+
+	@Test
+	void acos() {
+	}
+
+	@Test
+	void testAcos() {
+	}
+
+	@Test
+	void testAcos1() {
+	}
+
+	@Test
+	void atan() {
+	}
+
+	@Test
+	void testAtan() {
+	}
+
+	@Test
+	void testAtan1() {
+	}
+
+	@Test
+	void asinh() {
+	}
+
+	@Test
+	void testAsinh() {
+	}
+
+	@Test
+	void acosh() {
+	}
+
+	@Test
+	void testAcosh() {
+	}
+
+	@Test
+	void atanh() {
+	}
+
+	@Test
+	void testAtanh() {
+	}
+
+	@Test
+	void atan2() {
+	}
+
+	@Test
+	void testAtan2() {
+	}
+
+	@Test
+	void cot() {
+	}
+
+	@Test
+	void testCot() {
+	}
+
+	@Test
+	void acot() {
+	}
+
+	@Test
+	void testAcot() {
+	}
+
+	@Test
+	void acoth() {
+	}
+
+	@Test
+	void testAcoth() {
+	}
+
+	@Test
+	void coth() {
+	}
+
+	@Test
+	void testCoth() {
+	}
+
+	@Test
+	void combination() {
+	}
+
+	@Test
+	void testCombination() {
+	}
+
+	@Test
+	void permutation() {
+	}
+
+	@Test
+	void testPermutation() {
+	}
+
+	@Test
+	void polarToCartesianCoordinates() {
+	}
+
+	@Test
+	void testPolarToCartesianCoordinates() {
+	}
+
+	@Test
+	void cartesianToPolarCoordinates() {
+	}
+
+	@Test
+	void modulo() {
+	}
+
+	@Test
+	void gcd() {
+	}
+
+	@Test
+	void lcm() {
+	}
+
+	@Test
+	void testLcm() {
+	}
+
+	@Test
+	void testCartesianToPolarCoordinates() {
+	}
+
+	@Test
+	void formatToLocale() {
+	}
+
+	@Test
+	void formatWithGrouping() {
+	}
+
+	@Test
+	void setCalculatorEngineDivisionPrecision() {
+	}
+
+	@Test
+	void setCalculatorEngine() {
+	}
+
+	@Test
+	void setTrigonometricMode() {
+	}
+
+	@Test
+	void toDegrees() {
+	}
+
+	@Test
+	void toRadians() {
+	}
+
+	@Test
+	void isEqualTo() {
+	}
+
+	@Test
+	void isLessThan() {
+	}
+
+	@Test
+	void isLessThanOrEqualTo() {
+	}
+
+	@Test
+	void isGreaterThan() {
+	}
+
+	@Test
+	void isGreaterThanOrEqualTo() {
+	}
+
+	@Test
+	void intValue() {
+	}
+
+	@Test
+	void longValue() {
+	}
+
+	@Test
+	void floatValue() {
+	}
+
+	@Test
+	void doubleValue() {
+	}
+
+	@Test
+	void min() {
+	}
+
+	@Test
+	void max() {
+	}
+
+	@Test
+	void hasDecimals() {
+	}
+
+	@Test
+	void testToString() {
+	}
+
+	@Test
+	void compareTo() {
+	}
+
+	@Test
+	void testClone() {
+	}
+
+	@Test
+	void getLocale() {
+	}
+
+	@Test
+	void getValueBeforeDecimal() {
+	}
+
+	@Test
+	void getValueAfterDecimal() {
+	}
+
+	@Test
+	void isNegative() {
+	}
+
+	@Test
+	void getCalculatorEngine() {
+	}
+
+	@Test
+	void getTrigonometricMode() {
+	}
+
+	@Test
+	void getMathContext() {
+	}
+
+	@Test
+	void builder() {
 	}
 
 }
