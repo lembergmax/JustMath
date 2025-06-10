447f9e1b4d1fbe00e6e73a24a066e1de385be84b
Max Lemberg <max.lemberg2402@gmail.com>
2025-06-10 17:06:09 +0200

    added trigonometric functions to TrigonometricMath, including sin, cos, tan, and cot with support for BigNumber values

--- src/main/java/com/mlprograms/justmath/bignumber/internal/math/TrigonometricMath.java
+++ src/main/java/com/mlprograms/justmath/bignumber/internal/math/TrigonometricMath.java
@@ -10,23 +10,109 @@ import java.util.Locale;
 
 import static com.mlprograms.justmath.bignumber.internal.math.MathHelper.convertAngle;
 
+/**
+ * Provides trigonometric functions operating on BigNumber values.
+ * Supports angle inputs in degrees or radians, controlled by {@link TrigonometricMode}.
+ */
 public class TrigonometricMath {
 
+	/**
+	 * Calculates the sine of the given angle.
+	 * <p>
+	 * Mathematically, the sine function is defined as:
+	 * <pre>
+	 * sin(θ) = opposite / hypotenuse
+	 * </pre>
+	 * where θ is the angle in radians or degrees. This method converts the angle to radians if necessary.
+	 *
+	 * @param angle
+	 * 	the angle for which to compute sine
+	 * @param mathContext
+	 * 	the {@link MathContext} controlling precision and rounding
+	 * @param trigonometricMode
+	 * 	the angle measurement mode (DEG for degrees, RAD for radians)
+	 * @param locale
+	 * 	the locale used for number formatting
+	 *
+	 * @return the sine of the angle as a {@link BigNumber}
+	 */
 	public static BigNumber sin(BigNumber angle, MathContext mathContext, TrigonometricMode trigonometricMode, Locale locale) {
 		BigDecimal radians = convertAngle(angle, mathContext, trigonometricMode, locale);
 		return new BigNumber(BigDecimalMath.sin(radians, mathContext).toPlainString(), locale);
 	}
 
+	/**
+	 * Calculates the cosine of the given angle.
+	 * <p>
+	 * Mathematically, the cosine function is defined as:
+	 * <pre>
+	 * cos(θ) = adjacent / hypotenuse
+	 * </pre>
+	 * where θ is the angle in radians or degrees. This method converts the angle to radians if necessary.
+	 *
+	 * @param angle
+	 * 	the angle for which to compute cosine
+	 * @param mathContext
+	 * 	the {@link MathContext} controlling precision and rounding
+	 * @param trigonometricMode
+	 * 	the angle measurement mode (DEG for degrees, RAD for radians)
+	 * @param locale
+	 * 	the locale used for number formatting
+	 *
+	 * @return the cosine of the angle as a {@link BigNumber}
+	 */
 	public static BigNumber cos(BigNumber angle, MathContext mathContext, TrigonometricMode trigonometricMode, Locale locale) {
 		BigDecimal radians = convertAngle(angle, mathContext, trigonometricMode, locale);
 		return new BigNumber(BigDecimalMath.cos(radians, mathContext).toPlainString(), locale);
 	}
 
+	/**
+	 * Calculates the tangent of the given angle.
+	 * <p>
+	 * Mathematically, the tangent function is defined as:
+	 * <pre>
+	 * tan(θ) = sin(θ) / cos(θ)
+	 * </pre>
+	 * where θ is the angle in radians or degrees. This method converts the angle to radians if necessary.
+	 * Note that tangent is undefined where cosine is zero.
+	 *
+	 * @param angle
+	 * 	the angle for which to compute tangent
+	 * @param mathContext
+	 * 	the {@link MathContext} controlling precision and rounding
+	 * @param trigonometricMode
+	 * 	the angle measurement mode (DEG for degrees, RAD for radians)
+	 * @param locale
+	 * 	the locale used for number formatting
+	 *
+	 * @return the tangent of the angle as a {@link BigNumber}
+	 */
 	public static BigNumber tan(BigNumber angle, MathContext mathContext, TrigonometricMode trigonometricMode, Locale locale) {
 		BigDecimal radians = convertAngle(angle, mathContext, trigonometricMode, locale);
 		return new BigNumber(BigDecimalMath.tan(radians, mathContext).toPlainString(), locale);
 	}
 
+	/**
+	 * Calculates the cotangent of the given angle.
+	 * <p>
+	 * Mathematically, the cotangent function is defined as:
+	 * <pre>
+	 * cot(θ) = 1 / tan(θ) = cos(θ) / sin(θ)
+	 * </pre>
+	 * where θ is the angle in radians or degrees. This method converts the angle to radians if necessary.
+	 * Cotangent is undefined where sine is zero.
+	 *
+	 * @param angle
+	 * 	the angle for which to compute cotangent
+	 * @param mathContext
+	 * 	the {@link MathContext} controlling precision and rounding
+	 * @param trigonometricMode
+	 * 	the angle measurement mode (DEG for degrees, RAD for radians)
+	 * @param locale
+	 * 	the locale used for number formatting
+	 *
+	 * @return the cotangent of the angle as a {@link BigNumber}
+	 */
 	public static BigNumber cot(BigNumber angle, MathContext mathContext, TrigonometricMode trigonometricMode, Locale locale) {
 		BigDecimal radians = convertAngle(angle, mathContext, trigonometricMode, locale);
 		return new BigNumber(BigDecimalMath.cot(radians, mathContext).toPlainString(), locale);
