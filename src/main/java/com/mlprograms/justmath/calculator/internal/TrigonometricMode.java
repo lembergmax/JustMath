4edcfe310c7dbfc56478d72d1e0ad73106e5d1b7
Max Lemberg <max.lemberg2402@gmail.com>
2025-06-15 14:37:36 +0200

    refactor TrigonometricMode: add documentation for trigonometric modes enum

--- src/main/java/com/mlprograms/justmath/calculator/internal/TrigonometricMode.java
+++ src/main/java/com/mlprograms/justmath/calculator/internal/TrigonometricMode.java
@@ -1,5 +1,10 @@
 package com.mlprograms.justmath.calculator.internal;
 
+/**
+ * Enum representing the trigonometric modes supported by the calculator.
+ * DEG - Degrees mode.
+ * RAD - Radians mode.
+ */
 public enum TrigonometricMode {
 
 	DEG, RAD;
