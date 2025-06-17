b7a4baa87efc6cff03d9df001ab2d7cd5fc0437c
max.lemberg <max.lemberg2402@gmail.com>
2025-06-17 14:02:54 +0200

    some good changes

--- src/main/java/com/mlprograms/justmath/bignumber/internal/math/InverseHyperbolicTrigonometricMath.java
+++ src/main/java/com/mlprograms/justmath/bignumber/internal/math/InverseHyperbolicTrigonometricMath.java
@@ -4,9 +4,11 @@ import ch.obermuhlner.math.big.BigDecimalMath;
 import com.mlprograms.justmath.bignumber.BigNumber;
 import lombok.NonNull;
 
+import java.math.BigDecimal;
 import java.math.MathContext;
 import java.util.Locale;
 
+import static com.mlprograms.justmath.bignumber.internal.BigNumbers.NEGATIVE_ONE;
 import static com.mlprograms.justmath.bignumber.internal.BigNumbers.ONE;
 
 /**
@@ -36,115 +38,93 @@ import static com.mlprograms.justmath.bignumber.internal.BigNumbers.ONE;
  */
 public class InverseHyperbolicTrigonometricMath {
 
-	/**
-	 * Calculates the inverse hyperbolic sine (area hyperbolic sine) of the given argument.
-	 * <p>
-	 * The inverse hyperbolic sine is defined as:
-	 * <pre>
-	 * asinh(x) = ln(x + sqrt(x^2 + 1))
-	 * </pre>
-	 *
-	 * @param argument
-	 * 	the input value for which to compute asinh
-	 * @param mathContext
-	 * 	the {@link MathContext} specifying precision and rounding behavior
-	 * @param locale
-	 * 	the {@link Locale} used for formatting the resulting {@link BigNumber}
-	 *
-	 * @return a {@link BigNumber} representing asinh(argument) calculated with the specified precision
-	 */
-	public static BigNumber asinh(@NonNull final BigNumber argument, @NonNull final MathContext mathContext, @NonNull final Locale locale) {
-		return new BigNumber(BigDecimalMath.asinh(argument.toBigDecimal(), mathContext).toPlainString(), locale).trim();
-	}
+    /**
+     * Calculates the inverse hyperbolic sine (area hyperbolic sine) of the given argument.
+     * <p>
+     * The inverse hyperbolic sine is defined as:
+     * <pre>
+     * asinh(x) = ln(x + sqrt(x^2 + 1))
+     * </pre>
+     *
+     * @param argument    the input value for which to compute asinh
+     * @param mathContext the {@link MathContext} specifying precision and rounding behavior
+     * @param locale      the {@link Locale} used for formatting the resulting {@link BigNumber}
+     * @return a {@link BigNumber} representing asinh(argument) calculated with the specified precision
+     */
+    public static BigNumber asinh(@NonNull final BigNumber argument, @NonNull final MathContext mathContext, @NonNull final Locale locale) {
+        return new BigNumber(BigDecimalMath.asinh(argument.toBigDecimal(), mathContext).toPlainString(), locale).trim();
+    }
 
-	/**
-	 * Calculates the inverse hyperbolic cosine (area hyperbolic cosine) of the given argument.
-	 * <p>
-	 * The inverse hyperbolic cosine is defined as:
-	 * <pre>
-	 * acosh(x) = ln(x + sqrt(x^2 - 1))
-	 * </pre>
-	 * <p>
-	 * Domain restriction: {@code x >= 1}.
-	 *
-	 * @param argument
-	 * 	the input value for which to compute acosh
-	 * @param mathContext
-	 * 	the {@link MathContext} specifying precision and rounding behavior
-	 * @param locale
-	 * 	the {@link Locale} used for formatting the resulting {@link BigNumber}
-	 *
-	 * @return a {@link BigNumber} representing acosh(argument) calculated with the specified precision
-	 *
-	 * @throws ArithmeticException
-	 * 	if the argument is outside the domain (absolute value >= 1)
-	 */
-	public static BigNumber acosh(@NonNull final BigNumber argument, @NonNull final MathContext mathContext, @NonNull final Locale locale) {
-		if (argument.isLessThan(ONE)) {
-			throw new IllegalArgumentException("argument must be greater than 1");
-		}
+    /**
+     * Calculates the inverse hyperbolic cosine (area hyperbolic cosine) of the given argument.
+     * <p>
+     * The inverse hyperbolic cosine is defined as:
+     * <pre>
+     * acosh(x) = ln(x + sqrt(x^2 - 1))
+     * </pre>
+     * <p>
+     * Domain restriction: {@code x >= 1}.
+     *
+     * @param argument    the input value for which to compute acosh
+     * @param mathContext the {@link MathContext} specifying precision and rounding behavior
+     * @param locale      the {@link Locale} used for formatting the resulting {@link BigNumber}
+     * @return a {@link BigNumber} representing acosh(argument) calculated with the specified precision
+     * @throws ArithmeticException if the argument is outside the domain (absolute value >= 1)
+     */
+    public static BigNumber acosh(@NonNull final BigNumber argument, @NonNull final MathContext mathContext, @NonNull final Locale locale) {
+        if (argument.isLessThan(ONE)) {
+            throw new IllegalArgumentException("argument must be greater than 1");
+        }
 
-		return new BigNumber(BigDecimalMath.acosh(argument.toBigDecimal(), mathContext).toPlainString(), locale).trim();
-	}
+        return new BigNumber(BigDecimalMath.acosh(argument.toBigDecimal(), mathContext).toPlainString(), locale).trim();
+    }
 
-	/**
-	 * Calculates the inverse hyperbolic tangent (area hyperbolic tangent) of the given argument.
-	 * <p>
-	 * The inverse hyperbolic tangent is defined as:
-	 * <pre>
-	 * atanh(x) = 0.5 * ln((1 + x) / (1 - x))
-	 * </pre>
-	 * <p>
-	 * Domain restriction: {@code |x| < 1}.
-	 *
-	 * @param argument
-	 * 	the input value for which to compute atanh
-	 * @param mathContext
-	 * 	the {@link MathContext} specifying precision and rounding behavior
-	 * @param locale
-	 * 	the {@link Locale} used for formatting the resulting {@link BigNumber}
-	 *
-	 * @return a {@link BigNumber} representing atanh(argument) calculated with the specified precision
-	 *
-	 * @throws ArithmeticException
-	 * 	if the argument is outside the domain (absolute value >= 1)
-	 */
-	public static BigNumber atanh(@NonNull final BigNumber argument, @NonNull final MathContext mathContext, @NonNull final Locale locale) {
-		if (argument.isLessThanOrEqualTo(ONE)) {
-			throw new IllegalArgumentException("argument must be greater than 1");
-		}
+    /**
+     * Calculates the inverse hyperbolic tangent (area hyperbolic tangent) of the given argument.
+     * <p>
+     * The inverse hyperbolic tangent is defined as:
+     * <pre>
+     * atanh(x) = 0.5 * ln((1 + x) / (1 - x))
+     * </pre>
+     * <p>
+     * Domain restriction: {@code |x| < 1} and {@code |x| > 1}.
+     *
+     * @param argument    the input value for which to compute atanh
+     * @param mathContext the {@link MathContext} specifying precision and rounding behavior
+     * @param locale      the {@link Locale} used for formatting the resulting {@link BigNumber}
+     * @return a {@link BigNumber} representing atanh(argument) calculated with the specified precision
+     * @throws ArithmeticException if the argument is outside the domain (absolute value >= 1)
+     */
+    public static BigNumber atanh(@NonNull final BigNumber argument, @NonNull final MathContext mathContext, @NonNull final Locale locale) {
+        if (argument.isGreaterThanOrEqualTo(ONE) || argument.isLessThanOrEqualTo(NEGATIVE_ONE)) {
+            throw new IllegalArgumentException("argument must be between 1 and -1");
+        }
 
-		return new BigNumber(BigDecimalMath.atanh(argument.toBigDecimal(), mathContext).toPlainString(), locale).trim();
-	}
+        return new BigNumber(BigDecimalMath.atanh(argument.toBigDecimal(), mathContext).toPlainString(), locale).trim();
+    }
 
-	/**
-	 * Calculates the inverse hyperbolic cotangent (area hyperbolic cotangent) of the given argument.
-	 * <p>
-	 * The inverse hyperbolic cotangent is defined as:
-	 * <pre>
-	 * acoth(x) = 0.5 * ln((x + 1) / (x - 1))
-	 * </pre>
-	 * <p>
-	 * Domain restriction: {@code |x| > 1}.
-	 *
-	 * @param argument
-	 * 	the input value for which to compute acoth
-	 * @param mathContext
-	 * 	the {@link MathContext} specifying precision and rounding behavior
-	 * @param locale
-	 * 	the {@link Locale} used for formatting the resulting {@link BigNumber}
-	 *
-	 * @return a {@link BigNumber} representing acoth(argument) calculated with the specified precision
-	 *
-	 * @throws ArithmeticException
-	 * 	if the argument is outside the domain (absolute value <= 1)
-	 */
-	public static BigNumber acoth(@NonNull final BigNumber argument, @NonNull final MathContext mathContext, @NonNull final Locale locale) {
-		if (argument.isLessThanOrEqualTo(ONE)) {
-			throw new IllegalArgumentException("argument must be greater than 1");
-		}
+    /**
+     * Calculates the inverse hyperbolic cotangent (area hyperbolic cotangent) of the given argument.
+     * <p>
+     * The inverse hyperbolic cotangent is defined as:
+     * <pre>
+     * acoth(x) = 0.5 * ln((x + 1) / (x - 1))
+     * </pre>
+     * <p>
+     * Domain restriction: {@code |x| > 1}.
+     *
+     * @param argument    the input value for which to compute acoth
+     * @param mathContext the {@link MathContext} specifying precision and rounding behavior
+     * @param locale      the {@link Locale} used for formatting the resulting {@link BigNumber}
+     * @return a {@link BigNumber} representing acoth(argument) calculated with the specified precision
+     * @throws ArithmeticException if the argument is outside the domain (absolute value <= 1)
+     */
+    public static BigNumber acoth(@NonNull final BigNumber argument, @NonNull final MathContext mathContext, @NonNull final Locale locale) {
+        if (argument.isLessThanOrEqualTo(ONE)) {
+            throw new IllegalArgumentException("argument must be greater than 1");
+        }
 
-		return new BigNumber(BigDecimalMath.acoth(argument.toBigDecimal(), mathContext).toPlainString(), locale).trim();
-	}
+        return new BigNumber(BigDecimalMath.acoth(argument.toBigDecimal(), mathContext).toPlainString(), locale).trim();
+    }
 
 }
