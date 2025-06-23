992cf97e1c8a0b64907bdd320b97526234156017
max.lemberg <max.lemberg2402@gmail.com>
2025-06-23 13:40:54 +0200

    feat: add polar to cartesian coordinate conversion and improve BigNumber parsing

--- src/main/java/com/mlprograms/justmath/Main.java
+++ src/main/java/com/mlprograms/justmath/Main.java
@@ -1,21 +1,31 @@
 package com.mlprograms.justmath;
 
 import com.mlprograms.justmath.bignumber.BigNumber;
+import com.mlprograms.justmath.bignumber.BigNumberCoordinate;
+import com.mlprograms.justmath.bignumber.math.CoordinateConversionMath;
 import com.mlprograms.justmath.calculator.CalculatorEngine;
 import com.mlprograms.justmath.calculator.internal.TrigonometricMode;
 
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        BigNumber bigNumber1 = new BigNumber("12.3");
+        BigNumber bigNumber2 = new BigNumber("45.7");
+        BigNumberCoordinate coordinate = CoordinateConversionMath.polarToCartesianCoordinates(bigNumber1, bigNumber2, new MathContext(10, RoundingMode.HALF_UP), TrigonometricMode.DEG, Locale.GERMANY);
+        System.out.println(coordinate);
+
         testStation();
 
     }
 
     private static void testStation() {
-        System.out.println("Welcome to JustMath Calculator!");
+        System.out.println("Welcome to   JustMath Calculator!");
         System.out.println("Type your mathematical expression below:");
         Scanner scanner = new Scanner(System.in);
 
