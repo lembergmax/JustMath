58729042887bb4ce2ac800c81afe0339c62ffae4
Max Lemberg <max.lemberg2402@gmail.com>
2025-07-14 20:25:03 +0200

    feat: add ThreeArgumentSummationFunctionOperation interface for advanced summation operations

---
+++ src/main/java/com/mlprograms/justmath/calculator/internal/token/expressionelements/operations/ThreeArgumentSummationFunctionOperation.java
@@ -0,0 +1,15 @@
+package com.mlprograms.justmath.calculator.internal.token.expressionelements.operations;
+
+
+import com.mlprograms.justmath.bignumber.BigNumber;
+import com.mlprograms.justmath.calculator.internal.TrigonometricMode;
+
+import java.math.MathContext;
+import java.util.Locale;
+
+@FunctionalInterface
+public interface ThreeArgumentSummationFunctionOperation {
+
+	BigNumber apply(BigNumber a, BigNumber b, String c, MathContext mathContext, TrigonometricMode trigonometricMode, Locale locale);
+
+}
