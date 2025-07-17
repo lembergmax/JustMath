c224afb17a2f52e79523873beef365b95449b49a
Max Lemberg <max.lemberg2402@gmail.com>
2025-07-17 14:50:04 +0200

    refactor: add SummationToken class to represent summation expressions in calculations

---
+++ src/main/java/com/mlprograms/justmath/calculator/internal/token/SummationToken.java
@@ -0,0 +1,41 @@
+package com.mlprograms.justmath.calculator.internal.token;
+
+import lombok.Getter;
+
+import java.util.List;
+
+/**
+ * Represents a summation token in a mathematical expression.
+ * A summation token contains a start value, an end value, and a list of tokens representing the expression to be evaluated.
+ */
+@Getter
+public class SummationToken extends Token {
+    private final String start;
+    private final String end;
+    private final List<Token> expressionTokens;
+
+    /**
+     * Creates a new summation token with the specified start value, end value, and expression tokens.
+     *
+     * @param functionSymbol the function symbol (e.g., "sum" or "∑")
+     * @param start the start value of the summation
+     * @param end the end value of the summation
+     * @param expressionTokens the list of tokens representing the expression to be evaluated
+     */
+    public SummationToken(String functionSymbol, String start, String end, List<Token> expressionTokens) {
+        super(Type.SUMMATION, functionSymbol);
+        this.start = start;
+        this.end = end;
+        this.expressionTokens = expressionTokens;
+    }
+
+    /**
+     * Returns a string representation of this summation token.
+     *
+     * @return the string representation
+     */
+    @Override
+    public String toString() {
+        return "SUMMATION(start=" + start + ", end=" + end + ", tokens=" + expressionTokens + ")";
+    }
+}
