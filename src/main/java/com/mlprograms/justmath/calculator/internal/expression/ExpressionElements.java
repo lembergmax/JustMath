6c0e71724f0827c153d347fe2876a4b2671e7480
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-09 10:40:57 +0200

    refactor: remove redundant final keyword in ExpressionElements

--- src/main/java/com/mlprograms/justmath/calculator/internal/expression/ExpressionElements.java
+++ src/main/java/com/mlprograms/justmath/calculator/internal/expression/ExpressionElements.java
@@ -109,7 +109,7 @@ public class ExpressionElements {
     public static final String FUNC_LCM = "LCM";
     public static final String FUNC_GCD = "GCD";
     //
-    public final static String FUNC_REC = "Rec";
+    public static final String FUNC_REC = "Rec";
     public static final String FUNC_POL = "Pol";
     //
     public static final String FUNC_RANDINT = "RandInt";
