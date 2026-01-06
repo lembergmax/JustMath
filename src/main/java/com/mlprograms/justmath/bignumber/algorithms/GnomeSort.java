95764c953122f7350157362084b079baddf0e580
max.lemberg <max.lemberg2402@gmail.com>
2026-01-06 10:28:05 +0100

    feat: implement GnomeSort algorithm for sorting BigNumber instances

---
+++ src/main/java/com/mlprograms/justmath/bignumber/algorithms/GnomeSort.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2026 Max Lemberg
+ *
+ * This file is part of JustMath.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.mlprograms.justmath.bignumber.algorithms;
+
+import com.mlprograms.justmath.bignumber.BigNumber;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.util.List;
+
+@NoArgsConstructor
+public class GnomeSort extends SortingAlgorithm {
+
+    /**
+     * Returns a new {@link List} of {@link BigNumber} instances sorted in ascending order.
+     * <p>
+     * The original list is not modified. If the input list is {@code null}, empty,
+     * or contains fewer than two elements, the original list is returned.
+     *
+     * <p>This implementation is stable: equal elements keep their original relative order.</p>
+     *
+     * @param bigNumbers the list of {@link BigNumber} values to sort
+     * @return a new sorted list of {@link BigNumber} values in ascending order
+     */
+    @Override
+    public List<BigNumber> sort(@NonNull final List<BigNumber> bigNumbers) {
+        if (!isListValid(bigNumbers)) {
+            return bigNumbers;
+        }
+
+        final List<BigNumber> sortedList = cloneList(bigNumbers);
+
+        int index = 1;
+        while (index < sortedList.size()) {
+            if (index == 0) {
+                index = 1;
+                continue;
+            }
+
+            final BigNumber left = sortedList.get(index - 1);
+            final BigNumber right = sortedList.get(index);
+
+            if (left.isLessThanOrEqualTo(right)) {
+                index++;
+                continue;
+            }
+
+            swap(sortedList, index, index - 1);
+            index--;
+        }
+
+        return sortedList;
+    }
+
+    private void swap(@NonNull final List<BigNumber> list, final int leftIndex, final int rightIndex) {
+        final BigNumber leftValue = list.get(leftIndex);
+        list.set(leftIndex, list.get(rightIndex));
+        list.set(rightIndex, leftValue);
+    }
+
+}
