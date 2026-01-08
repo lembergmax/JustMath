3d61a9d7f3cc17fc867ce286cb8fb6896f330ef1
max.lemberg <max.lemberg2402@gmail.com>
2026-01-08 12:06:27 +0100

    feat: add VisibleWorldBounds record for internal visible bounds representation in JavaFX view

---
+++ src/main/java/com/mlprograms/justmath/graphfx/planar/view/VisibleWorldBounds.java
@@ -0,0 +1,7 @@
+package com.mlprograms.justmath.graphfx.planar.view;
+
+/**
+ * Internal visible bounds container used by the JavaFX view.
+ */
+record VisibleWorldBounds(double minX, double maxX, double minY, double maxY) {
+}
